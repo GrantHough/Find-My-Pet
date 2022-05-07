51e4f13ae9d59f66bba8ab40154561d3876b35c8
Grant Hough <grant.l.hough@gmail.com>
2022-05-07 13:23:05 -0700

    starting firebase

---
+++ FindMyPet/Find My Pet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Find My Pet
+//
+//  Created by Grant Hough on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        WelcomePage()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
